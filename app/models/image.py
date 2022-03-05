faa694122ce1176bba4bf6907e06fa2ead42fc72
vernfongchao <Vernchao93@gmail.com>
2022-03-05 08:06:59 -0800

    fixed typo from User Model and added created and updated fields to seeder file

--- app/models/image.py
+++ app/models/image.py
@@ -10,4 +10,4 @@ class Image(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, nullable=False, server_default=db.func.now(), server_onupdate=db.func.now())
-    user = db.relationship("Image", back_populates="images")
+    user = db.relationship("User", back_populates="images")
